82a68e8d741a9297f4ce8301f7b9386b908cb26c
Wendy Shan <92752107+pengyin-shan@users.noreply.github.com>
2023-02-15 12:14:19 -0500

    Remove unscoped and test again
    debugging if the memory issue comes from the unscoped method

--- lib/translation_io/client/base_operation/dump_db_text_keys_step.rb
+++ lib/translation_io/client/base_operation/dump_db_text_keys_step.rb
@@ -39,7 +39,7 @@ module TranslationIO
           @db_fields.keys.each do |table_name|
             table = table_name.constantize
             @db_fields[table_name].each do |column_name|
-              db_strings = table.unscoped.distinct.pluck(column_name)
+              db_strings = table.distinct.pluck(column_name)
               entries += db_strings
             end
           end
